7bcfd8f846955dda9fe5ffa2e3b87afbd5a93f09
Aleksandr <alexander.stashchuk@gmail.com>
2023-06-10 15:48:06 +0900

    Work with if

---
+++ 005_Ifs/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter your name, please: ");
+string user = Console.ReadLine();
+if (user.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(user);
+}
